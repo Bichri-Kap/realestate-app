abd01784265dc4e8e828919945c6bbe693e91d1f
Bich'ri Kapelembe <bichrixkapelembe04@gmail.com>
2025-11-19 15:23:53 +0200

    Implement viewsets for UserProfile, Booking, Property, and Review; update URL routing and requirements

--- realestate/urls.py
+++ realestate/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from properties.views import PropertyViewSet
+from accounts.views import UserProfileViewSet
+from bookings.views import BookingViewSet
+from reviews.views import ReviewViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'properties', PropertyViewSet)
+router.register(r'users', UserProfileViewSet)
+router.register(r'bookings', BookingViewSet)
+router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
